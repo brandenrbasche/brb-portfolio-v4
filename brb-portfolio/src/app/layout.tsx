2c23a5014c8e7184b723e6692dc1f3ec9ce1e8fd
Branden Basche <brandenbasche@gmail.com>
2024-11-12 15:55:10 -0500

    updated global font to Geist Mono

--- brb-portfolio/src/app/layout.tsx
+++ brb-portfolio/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.className} antialiased`}
+        className={`${geistMono.className} antialiased`}
       >
         {children}
         <Dock />
